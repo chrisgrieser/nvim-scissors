9c029eea23c0a8e9b587e0c34b4f1ea192103440
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-01-06 14:01:12 +0100

    refactor(pickers): simplified picker selection logic

--- lua/scissors/picker/init.lua
+++ lua/scissors/picker/init.lua
@@ -5,7 +5,7 @@ local u = require("scissors.utils")
 
 ---@param snip SnippetObj
 ---@return string
-local function snipCreateDisplay(snip)
+local function snipDisplay(snip)
 	local snipName = u.snipDisplayName(snip)
 	local filename = vim.fs.basename(snip.fullPath):gsub("%.json$", "")
 	return ("%s\t\t[%s]"):format(snipName, filename)
@@ -14,18 +14,17 @@ end
 ---@param allSnippets SnippetObj[]
 function M.selectSnippet(allSnippets)
 	local prompt = "Select Snippet:"
+
 	local hasTelescope, _ = pcall(require, "telescope")
-	if hasTelescope then
-		require("scissors.picker.telescope").selectSnippet(allSnippets, snipCreateDisplay, prompt)
-	else
-		require("scissors.picker.vim-ui-select").selectSnippet(allSnippets, snipCreateDisplay, prompt)
-	end
+	local picker = hasTelescope and "telescope" or "vim-ui-select"
+
+	require("scissors.picker." .. picker).selectSnippet(allSnippets, snipDisplay, prompt)
 end
 --------------------------------------------------------------------------------
 
 ---@param item snipFile
 ---@return string
-local function fileCreateDisplay(item)
+local function fileDisplay(item)
 	local snippetDir = require("scissors.config").config.snippetDir
 	local relPath = item.path:sub(#snippetDir + 2)
 	local shortened = relPath:gsub("%.jsonc?$", "")
@@ -38,21 +37,9 @@ function M.addSnippet(allSnipFiles, bodyPrefill)
 	local prompt = "Select file for new snippet:"
 
 	local hasTelescope, _ = pcall(require, "telescope")
-	if hasTelescope then
-		require("scissors.picker.telescope").addSnippet(
-			allSnipFiles,
-			fileCreateDisplay,
-			prompt,
-			bodyPrefill
-		)
-	else
-		require("scissors.picker.vim-ui-select").addSnippet(
-			allSnipFiles,
-			fileCreateDisplay,
-			prompt,
-			bodyPrefill
-		)
-	end
+	local picker = hasTelescope and "telescope" or "vim-ui-select"
+
+	require("scissors.picker." .. picker).addSnippet(allSnipFiles, fileDisplay, prompt, bodyPrefill)
 end
 --------------------------------------------------------------------------------
 return M
