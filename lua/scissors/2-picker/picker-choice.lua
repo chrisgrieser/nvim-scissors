0b882ca3175296c74f2e115bf6e328f368391d8a
Iron-E <code.iron.e@gmail.com>
2025-12-27 15:43:27 -0500

    feat(picker): detect if fzf-lua is installed, and use it if so
    I followed how snacks and telescope were implemented here.

--- lua/scissors/2-picker/picker-choice.lua
+++ lua/scissors/2-picker/picker-choice.lua
@@ -8,11 +8,14 @@ function M.selectSnippet(allSnippets)
 	local picker = require("scissors.config").config.snippetSelection.picker
 	local hasTelescope, _ = pcall(require, "telescope")
 	local hasSnacks, _ = pcall(require, "snacks")
+	local hasFzfLua, _ = pcall(require, "fzf-lua")
 
 	if picker == "telescope" or (picker == "auto" and hasTelescope) then
 		require("scissors.2-picker.telescope").selectSnippet(allSnippets, prompt)
 	elseif picker == "snacks" or (picker == "auto" and hasSnacks) then
 		require("scissors.2-picker.snacks").selectSnippet(allSnippets, prompt)
+	elseif picker == "fzf-lua" or (picker == "auto" and hasFzfLua) then
+		require("scissors.2-picker.fzf-lua").selectSnippet(allSnippets, prompt)
 	else
 		require("scissors.2-picker.vim-ui-select").selectSnippet(allSnippets, prompt)
 	end
